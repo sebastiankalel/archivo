72a61738ac4c40ea555e7ab831081f08af2373f3
nbusuario05 <nbusuario05@nbusuario05.(none)>
2014-07-10 08:12:16 -0300

    Agrega metodo de instancia archivate en FileRecord

--- app/models/file_record.rb
+++ app/models/file_record.rb
@@ -1,16 +1,20 @@
 class FileRecord < ActiveRecord::Base
 	has_many :steps
 
-#	after_save :save_step
 	after_create :save_step
 
    def save_step
-  # 	  @step = Step.create({file_record_id:id,person_id:0,office_id:1})
-  #    @step.save
+
       step=Step.start
       step. file_record_id=id
       step.save
 
+   end
+
+   def archivate
+      step=Step.archivate
+      step.file_record_id=id
+      step.save
    end
 
 end
